2dee8c595e6441a46366db4640ece086e1c78c40
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-05-25 00:04:59 -0400

    add game loop

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -27,22 +25,66 @@ func main() {
 		log.Fatalf("invalid username: %v", err)
 	}
 
-	_, _, err = pubsub.DeclareAndBind(
+	// create new game state
+	gamestate := gamelogic.NewGameState(username)
+
+	// subscribe to pause queue
+	err = pubsub.SubscribeJSON(
 		conn,
 		routing.ExchangePerilDirect,
 		fmt.Sprintf("%s.%s", routing.PauseKey, username),
 		routing.PauseKey,
 		pubsub.QueueTypeTransient,
+		handlerPause(gamestate),
 	)
 	if err != nil {
-		log.Fatalf("unable to create new queue: %v", err)
+		log.Fatalf("unable to subscribe to pause queue: %v", err)
 	}
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+gameloop:
+	for {
 
-	fmt.Println("\nExiting...")
+		input := gamelogic.GetInput()
+
+		switch input[0] {
+		case "spawn":
+
+			err = gamestate.CommandSpawn(input)
+			if err != nil {
+				fmt.Printf("invalid spawn command: %v\n", err)
+				continue
+			}
+
+		case "move":
+
+			_, err := gamestate.CommandMove(input)
+			if err != nil {
+				fmt.Printf("invalid move command: %v\n", err)
+				continue
+			}
+			fmt.Println("Move successful!")
+
+		case "status":
+
+			gamestate.CommandStatus()
+
+		case "help":
+
+			gamelogic.PrintClientHelp()
+
+		case "spam":
+
+			fmt.Println("Spamming not allowed yet!")
+
+		case "quit":
+
+			gamelogic.PrintQuit()
+			break gameloop
+
+		default:
+			fmt.Printf("unknown command: %s", input[0])
+		}
+
+	}
 
 }
